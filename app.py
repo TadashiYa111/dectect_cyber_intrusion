bbb18a6b255a4bb2459c2ea0e3911010a660ded9
magic <magicjohnson.ca@gmail.com>
2024-06-01 01:47:14 +0900

    Load dataset selected specify columns and visualize the dataset

---
+++ app.py
@@ -0,0 +1,43 @@
+from sklearn.metrics import accuracy_score,f1_score,recall_score,precision_score
+from sklearn.model_selection import train_test_split
+import time
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier,VotingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score,f1_score,recall_score,precision_score
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+# Preprocess the Dataset
+data_frame = pd.read_csv('./SIMARGL2021.csv')
+
+selected_columns = [
+'DST_TOS', 'SRC_TOS', 'TCP_WIN_SCALE_OUT', 'TCP_WIN_SCALE_IN', 'TCP_FLAGS','TCP_WIN_MAX_OUT', 'PROTOCOL', 'TCP_WIN_MIN_OUT', 'TCP_WIN_MIN_IN','TCP_WIN_MAX_IN', 'LAST_SWITCHED', 'TCP_WIN_MSS_IN', 'TOTAL_FLOWS_EXP','FIRST_SWITCHED', 'FLOW_DURATION_MILLISECONDS', 'LABEL'
+]
+data_frame = data_frame[selected_columns]
+print(data_frame.head())
+
+# Remove Duplicated dataset
+print("Duplicated Values : ", data_frame.duplicated().sum())
+data_frame.drop_duplicates(inplace=True)
+print(data_frame.head())
+
+grouped_data = data_frame.groupby('LABEL').size()
+fig, ax = plt.subplots(1)
+ax.bar(grouped_data.index, grouped_data.values)
+ax.set(xlabel='LABEL', ylabel='Distinct Count')
+plt.xticks(rotation=90)
+plt.show()
+
+grouped_data = data_frame['LABEL'].value_counts()
+fig, ax = plt.subplots(figsize=(8,8))
+ax.pie(grouped_data.values, labels=grouped_data.index, autopct='%1.1f%%', textprops={'fontsize': 5})
+ax.set_title('Distribution of LABEL')
+plt.show()
+
+normalized_data = data_frame.copy()
+numerical_columns = normalized_data.select_dtypes(include=['float64', 'int64']).columns
+
+non_numerical_columns = normalized_data.select_dtypes(exclude=['float64', 'int64']).columns
